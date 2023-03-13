c26e88e360286780344e1b7e0bc2e04de58a55f8
Adel MEJRI <adel.mejri@talan.com>
2023-03-13 11:13:00 +0100

    secrets script v1

---
+++ charts/platform-labp7/templates/secrets-batch.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+#{{- $hdfs := template "hdfs-principal" . -}}
+#{{- $hive := template "hive-principal" . -}}
+#{{- $spark := template "spark-principal" . -}}
+#{{- $http := template "http-principal" . -}}
+kubectl -n {{ .Release.Namespace }} exec {{ template "hdfs-k8s.krb5.fullname" . }} -- kadmin.local -q #{{ printf "addprinc -randkey %s" $hdfs | quote}}
+kubectl -n {{ .Release.Namespace }} exec {{ template "hdfs-k8s.krb5.fullname" . }} -- kadmin.local -q #{{ printf "addprinc -randkey %s" $hive | quote}}
+kubectl -n {{ .Release.Namespace }} exec {{ template "hdfs-k8s.krb5.fullname" . }} -- kadmin.local -q #{{ printf "addprinc -randkey %s" $spark | quote}}
+kubectl -n {{ .Release.Namespace }} exec {{ template "hdfs-k8s.krb5.fullname" . }} -- kadmin.local -q #{{ printf "addprinc -randkey %s" $http | quote}}
