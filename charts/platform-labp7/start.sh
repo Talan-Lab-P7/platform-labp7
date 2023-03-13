add61f0819afe38d0dbf5d98ba8871a5bd71e0e3
Adel MEJRI <adel.mejri@talan.com>
2023-03-13 18:40:10 +0100

    secrets script v1

--- charts/platform-labp7/start.sh
+++ charts/platform-labp7/start.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 helm install platform-labp7 -n test  . --set tags.ha=false --set tags.simple=true --set tags.ranger=false --set global.namenodeHAEnabled=false --set hdfs-simple-namenode-k8s.nodeSelector.hdfs-namenode-selector=hdfs-namenode-0 --set global.kerberosEnabled=true
-sleep 5
+echo "waiting 10 seconds kerberos po creation -- $date"
+sleep 10
 kubectl get cm secrets-batch -o=jsonpath='{.data.script}'  -n test > /tmp/secrets.sh
 chmod u+x /tmp/secrets.sh
 sh /tmp/secrets.sh
