a49cb86bc55af6386d367e45c7f27bd4c84125a0
Adel MEJRI <adel.mejri@talan.com>
2023-03-14 15:43:02 +0100

    secrets script v1

--- charts/platform-labp7/start.sh
+++ charts/platform-labp7/start.sh
@@ -2,8 +2,8 @@
 helm install platform-labp7 -n test  . --set tags.ha=false --set tags.simple=true --set tags.ranger=false --set global.namenodeHAEnabled=false --set hdfs-simple-namenode-k8s.nodeSelector.hdfs-namenode-selector=hdfs-namenode-0 --set global.kerberosEnabled=true
 kubectl get cm secrets-batch -o=jsonpath='{.data.script}'  -n test > /tmp/secrets.sh
 chmod u+x /tmp/secrets.sh
-while ( $(kubectl get po -n test | grep krb5 | awk '{print $2}') != "1/1" ); do
-    echo "waiting 10 seconds for kerberos pod creation " date
+while [ $(kubectl get po -n test | grep krb5 | awk '{print $2}') != "1/1" ]; do
+    echo "waiting kerberos pod creation " $(date +'%Y-%m-%d # %H:%M:%S')
     sleep 5
 done
 sh /tmp/secrets.sh
