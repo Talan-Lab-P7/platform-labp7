f74bf4bbbb51644b2ca126ce4a3b3ba237274c72
Adel MEJRI <adel.mejri@talan.com>
2023-04-19 10:02:34 +0200

    update script

--- charts/platform-labp7/start.sh
+++ charts/platform-labp7/start.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-if [[ -v $1 ]]; then echo "Merci d'introduire le nom du namespace"; exit 1; fi
+if [[ $# -eq null  ]]; then echo "Merci d'introduire le nom du namespace"; exit 1; fi
 helm install platform-labp7 -n $1  . --set tags.ha=false --set tags.ranger=true --set tags.external=false --set global.namenodeHAEnabled=false --set hdfs-simple-namenode-k8s.nodeSelector.hdfs-namenode-selector=hdfs-namenode-0 --set global.kerberosEnabled=true
 kubectl get cm secrets-batch -o=jsonpath='{.data.script}'  -n $1 > /tmp/secrets.sh
 chmod u+x /tmp/secrets.sh
